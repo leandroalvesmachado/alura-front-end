b4025079b2c2c0b37504a5aa3fa7c9030da7ca4e
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-01-11 23:42:51 -0300

    Exibindo datas

--- javascript/js-na-web-armazenando-dados-no-navegador/main.js
+++ javascript/js-na-web-armazenando-dados-no-navegador/main.js
@@ -1,7 +1,7 @@
 import BotaoConClui from './componentes/concluiTarefa.js';
 import BotaoDeleta from './componentes/deletaTarefa.js';
 
-const criarTarefa = (evento) => {
+const handleNovoItem = (evento) => {
     // evitar o comportamento padrao do botão
     // no caso evitar o refresh após clica no botão submit
     evento.preventDefault();
@@ -10,18 +10,34 @@ const criarTarefa = (evento) => {
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
 
+    const calendario = document.querySelector('[data-form-date]');
+    const data = moment(calendario.value);
+    const dataFormatada = data.format('DD/MM/YYYY');
+
+    const dados = {
+        valor,
+        dataFormatada
+    };
+
+    const criaTarefa = criarTarefa(dados);
+
+    lista.appendChild(criaTarefa);
+    input.value = '';
+};
+
+const criarTarefa = ({ valor, dataFormatada }) => {
     const tarefa = document.createElement('li');
     tarefa.classList.add('task');
-    const conteudo = `<p class="content">${valor}</p>`;
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`;
 
     tarefa.innerHTML = conteudo;
     tarefa.appendChild(BotaoConClui());
     tarefa.appendChild(BotaoDeleta());
-    lista.appendChild(tarefa);
-    input.value = '';
+    
+    return tarefa;
 };
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
 // função por referencia
-novaTarefa.addEventListener('click', criarTarefa);
+novaTarefa.addEventListener('click', handleNovoItem);
